27e745239bc6e7fe7e58fe04582395bc513a77da
Sarojan-stha <myselfsarojan@gmail.com>
2025-11-30 16:28:56 +0545

    basic navbar and resumes section

---
+++ app/components/ResumeCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router";
+
+const ResumeCard = ({ resume }: { resume: Resume }) => {
+  return (
+    <Link to={`/resume/${resume.companyName}`} className="resume-card">
+      {resume.companyName}
+    </Link>
+  );
+};
+
+export default ResumeCard;
---
+++ app/types/index.ts
@@ -0,0 +1,16 @@
+interface Resume {
+  id: string;
+  companyName: string;
+  jobTitle: string;
+  imagePath: string;
+  resumePath: string;
+  feedback: Feedback;
+}
+interface Feedback {
+  overallScore: number;
+  ATS: { score: number; tips: string[] };
+  toneAndStyle: { score: number; tips: string[] };
+  content: { score: number; tips: string[] };
+  structure: { score: number; tips: string[] };
+  skills: { score: number; tips: string[] };
+}
